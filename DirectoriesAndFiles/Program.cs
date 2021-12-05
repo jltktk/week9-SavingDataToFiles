804548ba9a18a42b2d31e05a0828cdcfdfa6ae86
Jürgen Luht <jluht@tktk.ee>
2021-12-06 00:15:46 +0200

    no comment

---
+++ DirectoriesAndFiles/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoriesAndFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\...\samples";
+            Console.WriteLine("Enter director name:");
+            string newDirectory = Console.ReadLine();
+            Console.WriteLine("Enter file name:");
+            string fileName = Console.ReadLine();
+
+
+            if (Directory.Exists($"{rootDirectory}\\{newDirectory}") && File.Exists($"{rootDirectory}\\{newDirectory}\\{fileName}"))
+            {
+                Console.WriteLine($"Directory {newDirectory} and file with same name already exists at {rootDirectory}.");
+            }
+            else
+            {
+                Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+                File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}");
+            }      
+        }
+    }
+}
